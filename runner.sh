0812d1c468df13b52bcd6c28ce5d88c14ace182c
EinTropit <slavin216@gmail.com>
2023-04-22 23:59:28 +0300

    added parameter feature

--- runner.sh
+++ runner.sh
@@ -1,121 +1,75 @@
 #!/bin/bash
 
-MAX_TEST=3
-
-EX_TO_TEST=0
-
-# single test mode
-SINGLE_TEST_MODE=0
-TEST_NUMBER=1
-
+MAX_TEST=100
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
 NC='\033[0m'
 
-STATUS=0
+EXIT_STATUS=0
 FINAL_TEST_STATUS=(1 1 1 1 1 1)
 
-if [ $SINGLE_TEST_MODE == 0 ]; then
-	if [ $EX_TO_TEST == 0 ]; then
-		for ((i=1; i<6; i++)); do
-			LOOP_STATUS=0
-			for ((j=1; j<$MAX_TEST; j++)); do 
-				as "ex${i}.asm" "../ATAM_tests/ex${i}/ex${i}test${j}" -o merged.o
+if [[ $1 =~ ^[1-5]$ ]]; then
 
-				if [ -f "merged.o" ]; then
-					ld merged.o -o merged.out
-					if [ -f "merged.out" ]; then
-						timeout 60s ./merged.out	
-						if [ $? -eq 0 ]; then
-							echo -e "ex${i}test${j}: ${GREEN}PASS${NC}"
-							STATUS=0
-						else
-							echo -e "ex${i}test${j}: ${RED}FAIL${NC}"
-							STATUS=1
-							LOOP_STATUS=1
-						fi
-					else
-						echo -e "ex${i}test${j} (ld stage): ${RED}FAIL${NC}"
-						STATUS=1
-						LOOP_STATUS=1
-					fi
-				else
-					echo -e "ex${i}test${j} (as stage): ${RED}FAIL${NC}"		
-					STATUS=1
-					LOOP_STATUS=1
-				fi
-				rm merged.*
-			done
-			
-			if [ $LOOP_STATUS == 0 ]; then
-				FINAL_TEST_STATUS[i]=0
-			fi
-		done
-		
-		for ((i=1; i<6; i++)); do
-			if [ ${FINAL_TEST_STATUS[$i]} == 0 ]; then
-				echo -e "ex${i} all tests status: ${GREEN}PASSED${NC}"
-			else
-				echo -e "ex${i} all tests status: (at least one test) ${RED}FAILED${NC}"
-			fi
-		done
-	else
-		for ((j=1; j<$MAX_TEST; j++)); do 
-			as "ex${EX_TO_TEST}.asm" "../ATAM_tests/ex${EX_TO_TEST}/ex${EX_TO_TEST}test${j}" -o merged.o
-
-			if [ -f "merged.o" ]; then
-				ld merged.o -o merged.out
-				if [ -f "merged.out" ]; then
-					timeout 60s ./merged.out	
-					if [ $? -eq 0 ]; then
-						echo -e "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
-						STATUS=0
-					else
-						echo -e "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
-						STATUS=1
-					fi
-				else
-					echo -e "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
-					STATUS=1	
-				fi
-			else
-				echo -e "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
-				STATUS=1
-			fi
-			rm merged.*
-		done
-	fi
+	SINGLE_EX_MODE=1
+	EX_BEGIN=$1
+	EX_END=($1 + 1)
 else
-	if (($EX_TO_TEST >= 1 && $EX_TO_TEST <= 5 && $TEST_NUMBER >=1 && $TEST_NUMBER <= $MAX_TEST)); then
-		as "ex${EX_TO_TEST}.asm" "../ATAM_tests/ex${EX_TO_TEST}/ex${EX_TO_TEST}test${TEST_NUMBER}" -o merged.o
+	SINGLE_EX_MODE=0
+	EX_BEGIN=1
+	EX_END=6
+fi
+
+if [[ $2 =~ ^[1-$MAX_TEST]$ ]]; then
+	TEST_BEGIN=$2
+	TEST_END=($2 + 1)
+else
+	TEST_BEGIN=0
+	TEST_END=($MAX_TEST + 1)
+fi
+
+for ((i=$EX_BEGIN; i<$EX_END; i++)); do
+	STATUS=0
+	for ((j=$TEST_BEGIN; j<$TEST_END; j++)); do 
+		as "ex${i}.asm" "../ATAM_tests/ex${i}/ex${i}test${j}" -o merged.o
 
 		if [ -f "merged.o" ]; then
 			ld merged.o -o merged.out
 			if [ -f "merged.out" ]; then
 				timeout 60s ./merged.out	
 				if [ $? -eq 0 ]; then
-					echo -e "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
+					echo -e "ex${i}test${j}: ${GREEN}PASS${NC}"
 					STATUS=0
 				else
-					echo -e "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
+					echo -e "ex${i}test${j}: ${RED}FAIL${NC}"
 					STATUS=1
 				fi
 			else
-				echo -e "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
-				STATUS=1	
+				echo -e "ex${i}test${j} (ld stage): ${RED}FAIL${NC}"
+				STATUS=1
 			fi
 		else
-			echo -e "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
+			echo -e "ex${i}test${j} (as stage): ${RED}FAIL${NC}"		
 			STATUS=1
 		fi
 		rm merged.*
-	else
-		echo -e "${RED}ex number or test number problem${NC}"
-		echo -e "${YELLOW}make sure you use the right mode!${NC}"
-		STATUS=1
+	done
+	
+	if [ $STATUS == 0 ]; then
+		FINAL_TEST_STATUS[i]=0
 	fi
+done
+
+if [ $SINGLE_EX_MODE == 1 ]; then
+	for ((i=1; i<6; i++)); do
+		if [ ${FINAL_TEST_STATUS[$i]} == 0 ]; then
+			echo -e "ex${i} all tests status: ${GREEN}PASSED${NC}"
+		else
+			echo -e "ex${i} all tests status: (at least one test) ${RED}FAILED${NC}"
+			EXIT_STATUS=1
+		fi
+	done
 fi
-echo -e "${YELLOW}Terminating runner${NC}"
-exit ${STATUS}
+
+echo -e "Terminating runner"
+exit ${EXIT_STATUS}
