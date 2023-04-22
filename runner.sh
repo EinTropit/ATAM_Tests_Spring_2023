85fec2e1c877ea2bf4c19973b92b8bf853dda873
BugoDuks <102538314+BugoDuks@users.noreply.github.com>
2023-04-22 17:11:13 +0000

    Add files via upload

--- runner.sh
+++ runner.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MAX_TEST=100
+MAX_TEST=10
 
-EX_TO_TEST=0
+EX_TO_TEST=3
 
 # single test mode
 SINGLE_TEST_MODE=0
-TEST_NUMBER=0
+TEST_NUMBER=1
 
 
 RED='\033[0;31m'
@@ -16,101 +16,99 @@ NC='\033[0m'
 
 STATUS=0
 
-if [ SINGLE_TEST_MODE == 0]; then
-	if [ EX_TO_TEST == 0 ]; then
+if [ $SINGLE_TEST_MODE == 0 ]; then
+	if [ $EX_TO_TEST == 0 ]; then
 		for ((i=1; i<6; i++)); do
 			LOOP_STATUS=0
-			for ((j=1; j<MAX_TEST; j++)); do 
-				as "ex${i}.asm" "ex${i}test${j}" -o merged.o
+			for ((j=1; j<$MAX_TEST; j++)); do 
+				as "ex${i}.asm" "../ATAM_tests/ex${i}/ex${i}test${j}" -o merged.o
 
-				if [ -f "mergerd.o" ]; then
+				if [ -f "merged.o" ]; then
 					ld merged.o -o merged.out
 					if [ -f "merged.out" ]; then
 						timeout 60s ./merged.out	
 						if [ $? -eq 0 ]; then
-							echo "ex${i}test${j}: ${GREEN}PASS${NC}"
+							echo -e "ex${i}test${j}: ${GREEN}PASS${NC}"
 							STATUS=0
-							LOOP_STATUS=0
 						else
-							echo "ex${i}test${j}: ${RED}FAIL${NC}"
+							echo -e "ex${i}test${j}: ${RED}FAIL${NC}"
 							STATUS=1
-							LOOP_STATUS=0
+							LOOP_STATUS=1
 						fi
 					else
-						echo "ex${i}test${j} (ld stage): ${RED}FAIL${NC}"
+						echo -e "ex${i}test${j} (ld stage): ${RED}FAIL${NC}"
 						STATUS=1
-						LOOP_STATUS=0	
+						LOOP_STATUS=1
 					fi
 				else
-					echo "ex${i}test${j} (as stage): ${RED}FAIL${NC}"		
+					echo -e "ex${i}test${j} (as stage): ${RED}FAIL${NC}"		
 					STATUS=1
-					LOOP_STATUS=0
+					LOOP_STATUS=1
 				fi
 				rm merged.*
 			done
-			if [ LOOP_STATUS == 1 ]; then
-				echo "Passed all ex${i} tests? ${RED}NO ):${NC}"
+			if [ $LOOP_STATUS == 1 ]; then
+				echo -e "Passed all ex${i} tests? ${RED}NO ):${NC}"
 			else
-				echo "Passed all ex${i} tests? ${GREEN}YES (:${NC}"
+				echo -e "Passed all ex${i} tests? ${GREEN}YES (:${NC}"
 			fi
 		done
 	else
-		for ((j=1; j<MAX_TEST; j++)); do 
-			as "ex${EX_TO_TEST}.asm" "ex${i}test${j}" -o merged.o
+		for ((j=1; j<$MAX_TEST; j++)); do 
+			as "ex${EX_TO_TEST}.asm" "../ATAM_tests/ex${EX_TO_TEST}/ex${EX_TO_TEST}test${j}" -o merged.o
 
-			if [ -f "mergerd.o" ]; then
+			if [ -f "merged.o" ]; then
 				ld merged.o -o merged.out
 				if [ -f "merged.out" ]; then
 					timeout 60s ./merged.out	
 					if [ $? -eq 0 ]; then
-						echo "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
+						echo -e "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
 						STATUS=0
 					else
-						echo "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
+						echo -e "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
 						STATUS=1
 					fi
 				else
-					echo "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
+					echo -e "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
 					STATUS=1	
 				fi
 			else
-				echo "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
+				echo -e "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
 				STATUS=1
 			fi
 			rm merged.*
 		done
 	fi
 else
-	if ((EX_TO_TEST >= 1 && EX_TO_TEST <= 5 && TEST_NUMBER >=1 && TEST_NUMBER <= MAX_TEST)); then
-		as "ex${EX_TO_TEST}.asm" "ex${i}test${j}" -o merged.o
+	if (($EX_TO_TEST >= 1 && $EX_TO_TEST <= 5 && $TEST_NUMBER >=1 && $TEST_NUMBER <= $MAX_TEST)); then
+		as "ex${EX_TO_TEST}.asm" "../ATAM_tests/ex${EX_TO_TEST}/ex${EX_TO_TEST}test${TEST_NUMBER}" -o merged.o
 
-		if [ -f "mergerd.o" ]; then
+		if [ -f "merged.o" ]; then
 			ld merged.o -o merged.out
 			if [ -f "merged.out" ]; then
 				timeout 60s ./merged.out	
 				if [ $? -eq 0 ]; then
-					echo "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
+					echo -e "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
 					STATUS=0
 				else
-					echo "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
+					echo -e "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
 					STATUS=1
 				fi
 			else
-				echo "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
+				echo -e "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
 				STATUS=1	
 			fi
 		else
-			echo "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
+			echo -e "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
 			STATUS=1
 		fi
 		rm merged.*
 	else
-		echo "${RED}ex number or test number problem${NC}"
-		echo "${YELLOW}make sure you use the right mode!${NC}"
+		echo -e "${RED}ex number or test number problem${NC}"
+		echo -e "${YELLOW}make sure you use the right mode!${NC}"
 		STATUS=1
 	fi
 fi
-echo "${YELLOW}Terminating runner${NC}"
+echo -e "${YELLOW}Terminating runner${NC}"
 exit ${STATUS}
-
 
