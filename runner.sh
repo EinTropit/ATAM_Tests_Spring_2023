c4f82ebd7d00da7d0ce89a881fe118d0c970d515
Tony <79003103+EinTropit@users.noreply.github.com>
2023-04-22 19:26:53 +0300

    Add files via upload

---
+++ runner.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+MAX_TEST=100
+
+EX_TO_TEST=0
+
+# single test mode
+SINGLE_TEST_MODE=0
+TEST_NUMBER=0
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+STATUS=0
+
+if [ SINGLE_TEST_MODE == 0]; then
+	if [ EX_TO_TEST == 0 ]; then
+		for ((i=1; i<6; i++)); do
+			LOOP_STATUS=0
+			for ((j=1; j<MAX_TEST; j++)); do 
+				as "ex${i}.asm" "ex${i}test${j}" -o merged.o
+
+				if [ -f "mergerd.o" ]; then
+					ld merged.o -o merged.out
+					if [ -f "merged.out" ]; then
+						timeout 60s ./merged.out	
+						if [ $? -eq 0 ]; then
+							echo "ex${i}test${j}: ${GREEN}PASS${NC}"
+							STATUS=0
+							LOOP_STATUS=0
+						else
+							echo "ex${i}test${j}: ${RED}FAIL${NC}"
+							STATUS=1
+							LOOP_STATUS=0
+						fi
+					else
+						echo "ex${i}test${j} (ld stage): ${RED}FAIL${NC}"
+						STATUS=1
+						LOOP_STATUS=0	
+					fi
+				else
+					echo "ex${i}test${j} (as stage): ${RED}FAIL${NC}"		
+					STATUS=1
+					LOOP_STATUS=0
+				fi
+				rm merged.*
+			done
+			if [ LOOP_STATUS == 1 ]; then
+				echo "Passed all ex${i} tests? ${RED}NO ):${NC}"
+			else
+				echo "Passed all ex${i} tests? ${GREEN}YES (:${NC}"
+			fi
+		done
+	else
+		for ((j=1; j<MAX_TEST; j++)); do 
+			as "ex${EX_TO_TEST}.asm" "ex${i}test${j}" -o merged.o
+
+			if [ -f "mergerd.o" ]; then
+				ld merged.o -o merged.out
+				if [ -f "merged.out" ]; then
+					timeout 60s ./merged.out	
+					if [ $? -eq 0 ]; then
+						echo "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
+						STATUS=0
+					else
+						echo "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
+						STATUS=1
+					fi
+				else
+					echo "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
+					STATUS=1	
+				fi
+			else
+				echo "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
+				STATUS=1
+			fi
+			rm merged.*
+		done
+	fi
+else
+	if ((EX_TO_TEST >= 1 && EX_TO_TEST <= 5 && TEST_NUMBER >=1 && TEST_NUMBER <= MAX_TEST)); then
+		as "ex${EX_TO_TEST}.asm" "ex${i}test${j}" -o merged.o
+
+		if [ -f "mergerd.o" ]; then
+			ld merged.o -o merged.out
+			if [ -f "merged.out" ]; then
+				timeout 60s ./merged.out	
+				if [ $? -eq 0 ]; then
+					echo "ex${EX_TO_TEST}test${j}: ${GREEN}PASS${NC}"
+					STATUS=0
+				else
+					echo "ex${EX_TO_TEST}test${j}: ${RED}FAIL${NC}"
+					STATUS=1
+				fi
+			else
+				echo "ex${EX_TO_TEST}test${j} (ld stage): ${RED}FAIL${NC}"
+				STATUS=1	
+			fi
+		else
+			echo "ex${EX_TO_TEST}test${j} (as stage): ${RED}FAIL${NC}"		
+			STATUS=1
+		fi
+		rm merged.*
+	else
+		echo "${RED}ex number or test number problem${NC}"
+		echo "${YELLOW}make sure you use the right mode!${NC}"
+		STATUS=1
+	fi
+fi
+echo "${YELLOW}Terminating runner${NC}"
+exit ${STATUS}
+
+
