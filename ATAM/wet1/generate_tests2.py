74c0e3fede1232111bbdbe15ce25b10459af4c33
BugoDuks <bugoduks@gmail.com>
2023-04-25 10:28:33 +0300

    ex2 tests check overlap

--- ATAM/wet1/generate_tests2.py
+++ ATAM/wet1/generate_tests2.py
@@ -23,7 +23,7 @@ for m in range(REP_NUM):
         cmps = "  mov $destination, %rax\n"
 
         endTest = random.randint(1, 10)
-        if i < 5:
+        if i < 2:  # negative num
             num = random.randint(-MAX_ARR_DATA, 1)
             data += f"  num: .int {num}\n"
             data += "  source: .byte 0\n"
@@ -34,19 +34,67 @@ for m in range(REP_NUM):
             cmps += "  jne bad_exit\n"
             cmps += f"  cmpb ${endTest}, 4(%rax)\n"
             cmps += "  jne bad_exit\n"
-        else:
-            num = random.randint(0, MAX_ARR_SIZE)
+
+        elif i < 8:  # overlap source before destination
+            size1 = random.randint(1, MAX_ARR_SIZE//2)
+            size2 = random.randint(1, MAX_ARR_SIZE//2)
+            data += f"  num: .int {size1+size2}\n"
+            data += "  source: .byte "
+            for j in range(size1):
+                x = random.randint(0, MAX_ARR_DATA)
+                if not (j == 0):
+                    data += ","
+                data += f"{x}"
+                cmps += f"  cmpb ${x}, {j}(%rax)\n"
+                cmps += "  jne bad_exit\n"
+            data += "\n"
+
+            data += "  destination: .byte "
+            for j in range(size2):
+                x = random.randint(0, MAX_ARR_DATA)
+                if not (j == 0):
+                    data += ","
+                data += f"{x}"
+                cmps += f"  cmpb ${x}, {size1+j}(%rax)\n"
+                cmps += "  jne bad_exit\n"
+            data += "\n"
+            data += f"               .zero {size1}\n"
+            data += f"               .byte {endTest}\n"
+
+            cmps += f"  cmpb ${endTest}, {size1+size2}(%rax)\n"
+            cmps += "  jne bad_exit\n"
+
+        elif i < 14:  # overlap destination before source
+            size1 = random.randint(1, MAX_ARR_SIZE//2)
+            num = size1 + random.randint(0, MAX_ARR_SIZE//2)
             data += f"  num: .int {num}\n"
-            source = []
+            data += f"  destination: .zero {size1}\n"
+
             data += "  source: .byte "
             for j in range(num):
                 x = random.randint(0, MAX_ARR_DATA)
-                # source.append(x)
+                if j == (num-size1):
+                    endTest = x
+                if not (j == 0):
+                    data += ","
+                data += f"{x}"
+                cmps += f"  cmpb ${x}, {j}(%rax)\n"
+                cmps += "  jne bad_exit\n"
+            data += "\n"
+            cmps += f"  cmpb ${endTest}, {num}(%rax)\n"
+            cmps += "  jne bad_exit\n"
+
+        else:  # separate
+            num = random.randint(0, MAX_ARR_SIZE)
+            data += f"  num: .int {num}\n"
+            data += "  source: .byte "
+            for j in range(num):
+                x = random.randint(0, MAX_ARR_DATA)
                 data += f"{x},"
                 cmps += f"  cmpb ${x}, {j}(%rax)\n"
                 cmps += "  jne bad_exit\n"
-
             data += "0\n"
+
             data += "  destination:\n"
             if not num == 0:
                 data += f"              .zero {num}\n"
