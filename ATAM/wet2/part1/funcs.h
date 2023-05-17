aec31f45c641e69b7bcec08c5a82923ad0876948
EinTropit <slavin216@gmail.com>
2023-05-17 17:42:56 +0300

    cleanup

--- ATAM/wet2/part1/funcs.h
+++ ATAM/wet2/part1/funcs.h
@@ -5,11 +5,6 @@
 #include <stdio.h>
 #include "aux_code.h"
 
-void keyMixing_c_imp(uint8_t input[4][4], uint8_t key[4][4]);
-void byteSubstitution_c_imp(uint8_t input[4][4]);
-void shiftRows_c_imp(uint8_t input[4][4]);
-void cipher_c_imp(uint8_t input[][4][4], uint8_t key[4][4], uint8_t len);
-
 void keyMixing(uint8_t input[4][4], uint8_t key[4][4]);
 void byteSubstitution(uint8_t input[4][4]);
 void shiftRows(uint8_t input[4][4]);
