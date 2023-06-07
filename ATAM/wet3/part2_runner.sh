42445b18d911acfc4704999f069f3d4aa23fbfe1
BugoDuks <bugoduks@gmail.com>
2023-06-07 17:11:18 +0300

    fixed phdrs and premissions checks

--- ATAM/wet3/part2_runner.sh
+++ ATAM/wet3/part2_runner.sh
@@ -12,7 +12,7 @@ if [ -f "part2.exec" ]; then
     readelf -h ./part2.exec > ./part2_out_files/part2_h.out
     readelf -s part2.exec > ./part2_out_files/part2_s.out
     readelf -WS part2.exec > ./part2_out_files/part2_WS.out
-    readelf -Wl ./part2.exec > ./part2_out_files/test_12.out
+    readelf -Wl ./part2.exec > ./part2_out_files/part2_Wl.out
 
     cat ./part2_out_files/part2_s.out | grep -ho '000000000040000c     0 NOTYPE  GLOBAL DEFAULT    3 _hw3_unicorn' > ./part2_out_files/test_1.out
     cat ./part2_out_files/part2_s.out | grep -ho '0000000000400000     0 NOTYPE  GLOBAL DEFAULT    3 _start' > ./part2_out_files/test_2.out
@@ -25,8 +25,14 @@ if [ -f "part2.exec" ]; then
     cat ./part2_out_files/part2_WS.out | grep '.rodata           PROGBITS        0000000080000000' | grep -ho ' 000005 00   A  0   0  4' > ./part2_out_files/test_9.out
     cat ./part2_out_files/part2_WS.out | grep '.bss              NOBITS          0000000000060008' | grep -ho ' 000032 00  WA  0   0  4' > ./part2_out_files/test_10.out
     cat ./part2_out_files/part2_h.out | grep 'Entry point address:' > ./part2_out_files/test_11.out
+    cat ./part2_out_files/part2_Wl.out | grep ' 0x0000000000060000 0x0000000000060000 0x000005 0x00003a RW  ' > ./part2_out_files/test_12.out
+    cat ./part2_out_files/part2_Wl.out | grep ' 0x0000000000400000 0x0000000000400000 0x000018 0x000018  WE ' > ./part2_out_files/test_13.out
+    cat ./part2_out_files/part2_Wl.out | grep ' 0x0000000080000000 0x0000000080000000 0x000005 0x000005 R E ' > ./part2_out_files/test_14.out
+    cat ./part2_out_files/part2_Wl.out | grep 'There are 3 program headers, starting at offset 64' > ./part2_out_files/test_15.out
+    cat ./part2_out_files/part2_Wl.out | grep -v 'LOAD' > ./part2_out_files/test_15.out
 
-    for ((i=1; i<=12; i++)); do
+
+    for ((i=1; i<=15; i++)); do
         diff "./part2_exp_files/test_${i}.exp" "./part2_out_files/test_${i}.out" &>/dev/null
         if [ $? -eq 0 ]; then
             echo -e "test ${i}: ${GREEN}PASS${NC}"
