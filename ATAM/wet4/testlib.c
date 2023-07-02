ba53431c0d07ec982b029baa2923d3dbaeac0323
BugoDuks <bugoduks@gmail.com>
2023-07-02 17:41:27 +0300

    tests check rsp problem

--- ATAM/wet4/testlib.c
+++ ATAM/wet4/testlib.c
@@ -9,12 +9,12 @@ int recA(int x)
 {
     if (x <= 0)
         return 10;
-    return recB(x-1, x+3);
+    return recB(x-1, x+3) + 100;
 }
 
 int recB(int x, int y)
 {
     if (x <= 0)
         return y;
-    return recA(x-1);
+    return recA(x-1) + 100;
 }
